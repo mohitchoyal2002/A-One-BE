43413437120eb441dc5d4c7b0e166f24a3168306
= <=>
2026-02-22 23:02:52 +0530

    fixed migration issue

--- db/migrate/20260222124250_add_used_to_receipts.rb
+++ db/migrate/20260222124250_add_used_to_receipts.rb
@@ -1,15 +1,6 @@
 class AddUsedToReceipts < ActiveRecord::Migration[7.1]
   def up
     add_column :receipts, :used, :boolean, default: false, null: false
-
-    # Backfill: mark receipts as used if they have an approved reward application
-    execute <<-SQL
-      UPDATE receipts
-      SET used = 1
-      WHERE receipt_no IN (
-        SELECT receipt_no FROM reward_applications WHERE status = 'approved'
-      )
-    SQL
   end
 
   def down
